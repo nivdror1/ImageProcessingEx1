c38662f7e9606eafdfb02dcb050f687bff46b112
nivdror1 <nivdror1@gmail.com>
2017-10-29 19:37:02 +0200

    3.3 done

--- ex1.py
+++ ex1.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 from scipy.misc import imsave as imsave, imread as imread
 from skimage.color import rgb2gray
 
+# The matrix which is used in rgb2yiq conversion
+mat = np.array([[0.299, 0.587, 0.114], [0.596, -0.275, -0.321], [0.212, -0.523, 0.311]])
+
+# The matrix which is used in yiq2rgb conversion
+mat_t = np.linalg.inv(mat).T
+
 def read_image(filename, representation):
     """
     read the image and convert it to gray scale if necessary
@@ -43,14 +49,34 @@ def display_image(filename, representation):
     plt.show()
     return 0
 
+def rgb2yiq(imRGB):
+    '''
+    convert the image format from RGB to YIQ
+    :param imRGB: The image as 3-D matrix whose values are of RGB
+    :return: An image as 3-D matrix whose values are of YIQ
+    '''
+    imYIQ = np.dot(imRGB, mat.T.copy())
+    return imYIQ
 
-
+def yiq2rgb(imYIQ):
+    '''
+    convert the image format from YIQ to RGB
+    :param imRGB: The image as 3-D matrix whose values are of YIQ
+    :return: An image as 3-D matrix whose values are of RGB
+    '''
+    imRGB = np.dot(imYIQ,mat_t.copy())
+    return imRGB
 
 def main():
     name1 = "logo.jpg"
     name2 = "logoGray.jpg"
 
-    display_image(name1, 2)
+    #display_image(name1, 2)
+    imgRGB = read_image(name1,2)
+    imgYIQ = rgb2yiq(imgRGB)
+    im = yiq2rgb(imgYIQ)
+    plt.imshow(im)
+    plt.show()
 
 if __name__ == "__main__":
     main()
