8874f643f5ea67400146a264d3cfd57f9fc30aad
nivdror1 <nivdror1@gmail.com>
2017-11-05 15:46:54 +0200

    pursuing 3.4 and 3.5

--- ex1.py
+++ ex1.py
@@ -95,15 +95,16 @@ def equalize_image(hist_orig, im_gray, bins):
     # get the cumulative distribution function
     hist_eq = hist_orig.cumsum()
     # normalize
-    hist_eq = ((hist_eq / hist_eq[255]) * 255).astype(int)
+    hist_eq = ((hist_eq / hist_eq[255]) * 255).astype(np.uint8)
+
 
     im_eq = np.interp(im_gray.flatten(),bins[:-1],hist_eq)
     im_eq = im_eq.reshape(im_gray.shape)
     #im_eq = hist_eq[im_gray]
     h, b = np.histogram(im_eq,256,[0,256])
-    #plt.imshow(im_eq)
-    # plt.plot(hist_orig )
-    # plt.plot(h)
+    cdf = h.cumsum()
+
+    plt.plot(cdf)
     plt.show()
 
 
@@ -118,7 +119,7 @@ def histogram_equalize_rgb(im_orig):
     '''
     # transform to YIQ
     im_yiq = rgb2yiq(im_orig)
-    im_gray = ((np.asarray(im_yiq[:, :, 0]) * 255).round()).astype(int)
+    im_gray = ((np.asarray(im_yiq[:, :, 0]) * 255).round()).astype(np.uint8)
 
     # get the histogram of the origin image
     hist_orig, bins = get_hist_orig(im_yiq, True)
@@ -135,11 +136,11 @@ def histogram_equalize_rgb(im_orig):
 def histogram_equalize_gray(im_orig):
     '''
     Perform the histogram equalization algorithm for gray scale image
-    :param im_orig: The origin image
+    :param im_orig: The origin image..
     :return: hist_orig,hist_eq_im_eq
     '''
     hist_orig, bins = get_hist_orig(im_orig, False)
-    im_gray = ((np.asarray(im_orig) * 255).round()).astype(int)
+    im_gray = ((np.asarray(im_orig) * 255).round()).astype(np.uint8)
 
     # get the equalize histogram and image
     hist_eq, im_eq = equalize_image(hist_orig, im_gray, bins)
@@ -164,36 +165,67 @@ def histogram_equalize(im_orig):
     return hist_orig, hist_eq, im_eq
 
 
-def restart_z(im_orig, n_quant, hist_orig):
-    cdf = hist_orig.cumsum()
+def restart_z( n_quant, hist_orig):
+    '''
+    set the z parameter to the initial value for the quantization algorithm
+    :param n_quant: the number of color in the new image
+    :param hist_orig: The histogram of the origin image
+    :return: the initial z array
+    '''
 
-    hist_segm_arr = np.arange(256) * (cdf[-1] / n_quant)
-    z = (np.argmin(np.abs(cdf[:, np.newaxis] - hist_segm_arr), axis=0))[:n_quant + 1]
+    cdf = hist_orig.cumsum()
+    hist_seg= np.arange(256) * (cdf[-1] / n_quant)
+    z = (np.argmin(np.abs(cdf[:, np.newaxis] - hist_seg), axis=0))[:n_quant + 1]
     z[n_quant] = 256.0
     return z
 
 def calculate_q(n_quant, q, z, hist_orig, bins):
+    '''
+    calculate the q parameter array i.e the gray scale values
+    :param n_quant: the number of color in the new image
+    :param q: the gray scale values
+    :param z: the segment of the partition of the histogram
+    :param hist_orig: The histogram of the origin image
+    :param bins: The gray scale palette
+    :return: The q parameter array
+    '''
     for segment in range(n_quant):
-        hist_seg = np.asarray(hist_orig[z[segment]: z[segment] + 1])
-        bins_seg = np.asarray(bins[z[segment]: z[segment] + 1])
-
+        #partition the array
+        hist_seg = np.asarray(hist_orig[z[segment]: z[segment + 1]])
+        bins_seg = np.asarray(bins[z[segment]: z[segment + 1]])
+        #calculate the q parameter
         segment_cdf = hist_seg.cumsum()
-
-        q[segment] = (bins_seg * hist_seg) / (segment_cdf[-1])
+        q[segment] = ((bins_seg * hist_seg).sum())/ (segment_cdf[-1])
     return q
 
 
 def error_calculation(n_quant ,z, q, hist_orig, bins):
+    '''
+    calculate the error per each iteration
+    :param n_quant: the number of color in the new image
+    :param z: the segment of the partition of the histogram
+    :param q: the gray scale values
+    :param hist_orig: The histogram of the origin image
+    :param bins: The gray scale palette
+    :return:
+    '''
     sum_err = 0
     for index in range(n_quant):
-        hist_seg = np.asarray(hist_orig[z[index]: z[index] + 1])
-        bins_seg = np.asarray(bins[z[index]: z[index] + 1])
-        sum_err += np.power((q[index] - bins_seg), 2) * hist_seg
-
+        # partition the array
+        hist_seg = np.asarray(hist_orig[z[index]: z[index + 1]])
+        bins_seg = np.asarray(bins[z[index]: z[index + 1]])
+        #calculate the error for each segment
+        sum_err += (np.power((q[index] - bins_seg), 2) * hist_seg).sum()
     return sum_err
 
-
 def get_lookup_table(n_quant, z, q):
+    '''
+    form a lookup table
+    :param n_quant: the number of color in the new image
+    :param z: the segment of the partition of the histogram
+    :param q: the gray scale values
+    :return: A lookup table
+    '''
     lut = np.arange(256)
     # form a lookup table
     for i in range(n_quant):
@@ -206,43 +238,47 @@ def quantize(im_orig, n_quant, n_iter):
     if len(im_orig.shape) == 3:
         im_yiq = rgb2yiq(im_orig)
         gray_channel = im_yiq[:, :, 0]
+        hist_orig, bins = get_hist_orig(im_yiq, True)
     else:
         gray_channel = im_orig
+        hist_orig, bins = get_hist_orig(im_yiq, False)
 
-    hist_orig, hist_eq, im_eq = histogram_equalize(im_orig)
-    plt.plot(hist_orig)
-    plt.show()
     # restart z
-    z = restart_z(im_orig, n_quant)
+    z = restart_z( n_quant, hist_orig)
 
     bins = np.arange(256)
-    q = np.arange(n_quant)
+    q = np.arange(n_quant).astype(np.float64)
     last_z = np.zeros(z.shape)
     error = []
 
     for it in range(n_iter):
         # q calculation
-        q = calculate_q(n_quant, z, q, hist_orig, bins)
+        q = calculate_q(n_quant, q, z, hist_orig, bins)
 
         #z calculation
-        for interval in range(n_quant):
-            z[interval] = (q[interval]+q[interval])/2
+        for interval in range(1, n_quant):
+
+            z[interval] = (q[interval - 1] + q[interval])/2
 
         #error calucation
-        error.append(error_calculation(n_quant,z ,q ,hist_orig, bins))
+        error.append(error_calculation(n_quant,z ,q.round() ,hist_orig, bins))
 
         #check if z has converged
-        if last_z ==z:
+        if np.array_equal(last_z,z):
             break
 
         #update last_z
-        last_z = z
+        last_z = np.copy(z)
     #form a lookup table
-    lut = get_lookup_table(n_quant, z, q)
+    lut = get_lookup_table(n_quant, z, q.round())
+
 
     #update the image
+    gray_channel = np.asarray(np.multiply(gray_channel, 255)).astype(int)
     im_quant = lut[gray_channel]
-
+    im_quant = np.divide(im_quant,255)
+    plt.plot(error)
+    plt.show()
     if len(im_orig.shape) == 3:
         im_yiq[:, :, 0] = im_quant
         im_quant = yiq2rgb(im_yiq)
@@ -251,16 +287,17 @@ def quantize(im_orig, n_quant, n_iter):
 
 
 def main():
-    name1 = "jerusalem.jpg"
+    name1 = "gray_orig.png"
     name2 = "logoGray.jpg"
 
     #display_image(name1, 2)
     imgRGB = read_image(name1,2)
     #display_image(name1, 1)
     #imsave("LowGray.jpg",imgRGB)
-    quantize(imgRGB,5, 20)
-    # hist_orig, hist_eq, im_eq = histogram_equalize(imgRGB)
-    # plt.imshow(im_eq)
+    list = quantize(imgRGB,10, 20)
+    plt.imshow(list[0].clip(0,1))
+    #hist_orig, hist_eq, im_eq = histogram_equalize(imgRGB)
+    #plt.imshow(im_eq)
     # plt.show()
 
 if __name__ == "__main__":
